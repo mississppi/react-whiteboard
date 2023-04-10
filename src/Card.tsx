99c7cec63a085f87cd1a30b3fd7b4e73eee7779a
mississppi <msspstudio@gmail.com>
2023-04-10 22:02:33 +0900

    first

---
+++ src/Card.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export type Card = {
+    id: number;
+    content: string;
+    x: number;
+    y: number;
+    editing: boolean;
+}
+
+const Card = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Card;
