42868f77bbd31face2ba1a32dedeee391d43a72f
mississppi <msspstudio@gmail.com>
2023-05-04 16:11:11 +0900

    add chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Card from './Card';
+import { ChakraProvider } from '@chakra-ui/react'
 import './App.css';
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <Card />
     </div>
+    </ChakraProvider>
   );
 }
 
